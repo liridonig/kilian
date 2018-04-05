e1fc81cc31f9b6326019a696e44e289d9d39874f
Liridon Gashi <37881325+liridonig@users.noreply.github.com>
2018-04-05 21:54:56 +0200

    Add files via upload

---
+++ kilian/scroll.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){  
+  $("#button").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 2000);
+  });
+});
+$(document).ready(function(){  
+  $("#button1").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 2000);
+  });
+});
+$(document).ready(function(){  
+  $("#button2").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 2000);
+  });
+});
+$(document).ready(function(){  
+  $("#button3").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 2000);
+  });
+});
+$(document).ready(function(){  
+  $("#button4").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 2000);
+  });
+});
